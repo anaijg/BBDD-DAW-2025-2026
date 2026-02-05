9f49da7612bc4046ece79105e79faaa3f6cf2fc7
anaijg <ana.jimenezgomez@educa.madrid.org>
2026-02-05 16:10:29 +0100

    añado consultas externas tienda

--- consultas/tienda/tienda_consultas_multitabla.sql
+++ consultas/tienda/tienda_consultas_multitabla.sql
@@ -1,19 +1,41 @@
 # composición interna (INNER JOIN)
-
+use tienda;
 # 1. Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los productos de la base de datos.
-#
+select p.nombre, p.precio, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id;
+
 # 2. Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los productos de la base de datos. Ordene el resultado por el nombre del fabricante, por orden alfabético.
-#
+select p.nombre, p.precio, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+order by f.nombre;
+
 # 3. Devuelve una lista con el identificador del producto, nombre del producto, identificador del fabricante y nombre del fabricante, de todos los productos de la base de datos.
-#
+select p.id, p.nombre, f.id, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+
 # 4. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más barato.
-#
+select p.nombre, p.precio, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+order by p.precio
+limit 1;
+
 # 5. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más caro.
-#
+
+
 # 6. Devuelve una lista de todos los productos del fabricante Lenovo.
-#
+
+
 # 7. Devuelve una lista de todos los productos del fabricante Crucial que tengan un precio mayor que 200€.
-#
+
+
 # 8. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy Seagate. Sin utilizar el operador IN.
 #
 # 9. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy Seagate. Utilizando el operador IN.
@@ -27,3 +49,10 @@
 # 13. Devuelve un listado con el identificador y el nombre de fabricante, solamente de aquellos fabricantes que tienen productos asociados en la base de datos.
 
 # composición externa (OUTER JOIN)
+# Resuelva todas las consultas utilizando las cláusulas LEFT JOIN y RIGHT JOIN.
+#
+# 1. Devuelve un listado de todos los fabricantes que existen en la base de datos, junto con los productos que tiene cada uno de ellos. El listado deberá mostrar también aquellos fabricantes que no tienen productos asociados.
+#
+# 2. Devuelve un listado donde sólo aparezcan aquellos fabricantes que no tienen ningún producto asociado.
+#
+# 3. ¿Pueden existir productos que no estén relacionados con un fabricante? Justifique su respuesta.
