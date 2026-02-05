c591dbc7c722c79be7940b7ab551d9e03d222689
anaijg <ana.jimenezgomez@educa.madrid.org>
2026-02-05 16:53:10 +0100

    asdf

--- consultas/tienda/tienda_consultas_multitabla.sql
+++ consultas/tienda/tienda_consultas_multitabla.sql
@@ -99,7 +99,15 @@ from producto as p
 # Resuelva todas las consultas utilizando las cláusulas LEFT JOIN y RIGHT JOIN.
 #
 # 1. Devuelve un listado de todos los fabricantes que existen en la base de datos, junto con los productos que tiene cada uno de ellos. El listado deberá mostrar también aquellos fabricantes que no tienen productos asociados.
+select f.nombre, p.nombre
+    from producto as p right join fabricante as f
+on p.id_fabricante = f.id;
 
 # 2. Devuelve un listado donde sólo aparezcan aquellos fabricantes que no tienen ningún producto asociado.
+select f.nombre, p.nombre
+    from producto as p right join fabricante as f
+on p.id_fabricante = f.id
+where p.nombre is null;
 
 # 3. ¿Pueden existir productos que no estén relacionados con un fabricante? Justifique su respuesta.
+# NO. Integridad referencial -> producto.id_fabricante es clave foránea y tiene necesariamente que tener un fabricante.id asociado como clave primaria. (esto viene de la cardinalidad 0..n <-> 1
