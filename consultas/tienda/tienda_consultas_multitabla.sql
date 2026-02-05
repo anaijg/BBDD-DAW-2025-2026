6c6760ad2e786c4d076d70829087bb3c942c97bd
anaijg <ana.jimenezgomez@educa.madrid.org>
2026-02-05 16:34:53 +0100

    consultas multitabla inner tienda resueltas

--- consultas/tienda/tienda_consultas_multitabla.sql
+++ consultas/tienda/tienda_consultas_multitabla.sql
@@ -17,7 +17,7 @@ order by f.nombre;
 select p.id, p.nombre, f.id, f.nombre
 from producto as p
          join fabricante as f
-              on p.id_fabricante = f.id
+              on p.id_fabricante = f.id;
 
 # 4. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más barato.
 select p.nombre, p.precio, f.nombre
@@ -28,31 +28,78 @@ order by p.precio
 limit 1;
 
 # 5. Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más caro.
-
+select p.nombre, p.precio, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+order by p.precio desc
+limit 1;
 
 # 6. Devuelve una lista de todos los productos del fabricante Lenovo.
-
+select p.nombre, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre = 'Lenovo';
 
 # 7. Devuelve una lista de todos los productos del fabricante Crucial que tengan un precio mayor que 200€.
+select p.nombre, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre = 'Crucial'
+  and p.precio > 200;
 
 
 # 8. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy Seagate. Sin utilizar el operador IN.
-#
+select p.nombre, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre = 'Asus'
+   or f.nombre = 'Hewlett-Packard'
+   or f.nombre = 'Seagate';
+
 # 9. Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packardy Seagate. Utilizando el operador IN.
-#
+select p.nombre, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre in ('Asus', 'Hewlett-Packard', 'Seagate');
+
 # 10. Devuelve un listado con el nombre y el precio de todos los productos de los fabricantes cuyo nombre termine por la vocal e.
-#
+select p.nombre, p.precio
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre like '%e';
+
 # 11. Devuelve un listado con el nombre y el precio de todos los productos cuyo nombre de fabricante contenga el carácter w en su nombre.
-#
+select p.nombre, p.precio, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre like '%w%';
+
 # 12. Devuelve un listado con el nombre de producto, precio y nombre de fabricante, de todos los productos que tengan un precio mayor o igual a 180€. Ordene el resultado en primer lugar por el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente)
-#
+select p.nombre, p.precio, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where p.precio >= 180
+order by p.precio desc , p.nombre;
+
 # 13. Devuelve un listado con el identificador y el nombre de fabricante, solamente de aquellos fabricantes que tienen productos asociados en la base de datos.
+select distinct f.id, f.nombre
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id;
 
 # composición externa (OUTER JOIN)
 # Resuelva todas las consultas utilizando las cláusulas LEFT JOIN y RIGHT JOIN.
 #
 # 1. Devuelve un listado de todos los fabricantes que existen en la base de datos, junto con los productos que tiene cada uno de ellos. El listado deberá mostrar también aquellos fabricantes que no tienen productos asociados.
-#
+
 # 2. Devuelve un listado donde sólo aparezcan aquellos fabricantes que no tienen ningún producto asociado.
-#
+
 # 3. ¿Pueden existir productos que no estén relacionados con un fabricante? Justifique su respuesta.
