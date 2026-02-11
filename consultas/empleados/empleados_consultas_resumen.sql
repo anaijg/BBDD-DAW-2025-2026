307cc1d309ce780d13115f6ae1400716c128b2d9
anaijg <ana.jimenezgomez@educa.madrid.org>
2026-02-11 20:06:48 +0100

    empleados consultas resumen enunciados

---
+++ consultas/empleados/empleados_consultas_resumen.sql
@@ -0,0 +1,24 @@
+# 1.2.6 Consultas resumen
+# 1. Calcula la suma del presupuesto de todos los departamentos.
+#
+# 2. Calcula la media del presupuesto de todos los departamentos.
+#
+# 3. Calcula el valor mínimo del presupuesto de todos los departamentos.
+#
+# 4. Calcula el nombre del departamento y el presupuesto que tiene asignado, del departamento con menor presupuesto.
+#
+# 5. Calcula el valor máximo del presupuesto de todos los departamentos.
+#
+# 6. Calcula el nombre del departamento y el presupuesto que tiene asignado, del departamento con mayor presupuesto.
+#
+# 7. Calcula el número total de empleados que hay en la tabla empleado.
+#
+# 8. Calcula el número de empleados que no tienen NULL en su segundo apellido.
+#
+# 9. Calcula el número de empleados que hay en cada departamento. Tienes que devolver dos columnas, una con el nombre del departamento y otra con el número de empleados que tiene asignados.
+#
+# 10. Calcula el nombre de los departamentos que tienen más de 2 empleados. El resultado debe tener dos columnas, una con el nombre del departamento y otra con el número de empleados que tiene asignados.
+#
+# 11. Calcula el número de empleados que trabajan en cada uno de los departamentos. El resultado de esta consulta también tiene que incluir aquellos departamentos que no tienen ningún empleado asociado.
+#
+# 12. Calcula el número de empleados que trabajan en cada unos de los departamentos que tienen un presupuesto mayor a 200000 euros.
