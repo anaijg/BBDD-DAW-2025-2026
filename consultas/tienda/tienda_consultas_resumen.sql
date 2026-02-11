573f84d0a91818ccd293aeedf36aea4c855d8863
anaijg <ana.jimenezgomez@educa.madrid.org>
2026-02-11 20:49:07 +0100

    ejercicios resueltos tienda resumen sin group by

--- consultas/tienda/tienda_consultas_resumen.sql
+++ consultas/tienda/tienda_consultas_resumen.sql
@@ -1,34 +1,103 @@
 # 1.1.6 Consultas resumen
+use tienda;
 # 1. Calcula el número total de productos que hay en la tabla productos.
-#
+select count(*)
+from producto;
+-- 11
 # 2. Calcula el número total de fabricantes que hay en la tabla fabricante.
-#
+select count(*)
+from fabricante;
+-- 9
 # 3. Calcula el número de valores distintos de identificador de fabricante aparecen en la tabla productos.
-#
+select count(distinct id_fabricante)
+from producto;
+-- 7
+
 # 4. Calcula la media del precio de todos los productos.
-#
+select avg(precio)
+from producto;
+-- 271.7236363636364
+
 # 5. Calcula el precio más barato de todos los productos.
-#
+select min(precio)
+from producto;
+-- 59.99
+
 # 6. Calcula el precio más caro de todos los productos.
-#
+select max(precio)
+from producto;
+-- 755
+
 # 7. Lista el nombre y el precio del producto más barato.
-#
+select nombre, precio
+from producto
+order by precio
+limit 1;
+-- Impresora HP Deskjet 3720,59.99
+
 # 8. Lista el nombre y el precio del producto más caro.
-#
+select nombre, precio
+from producto
+order by precio desc
+limit 1;
+-- GeForce GTX 1080 Xtreme,755
+
 # 9. Calcula la suma de los precios de todos los productos.
-#
+select sum(precio)
+from producto;
+-- 2988.96
+
 # 10. Calcula el número de productos que tiene el fabricante Asus.
-#
+select count(*)
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre = 'Asus';
+-- 2
+
 # 11. Calcula la media del precio de todos los productos del fabricante Asus.
-#
+select avg(precio)
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre = 'Asus';
+-- 223.995
+
 # 12. Calcula el precio más barato de todos los productos del fabricante Asus.
-#
+select min(precio)
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre = 'Asus';
+-- 202
+
 # 13. Calcula el precio más caro de todos los productos del fabricante Asus.
-#
+select max(precio)
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre = 'Asus';
+-- 245.99
+
 # 14. Calcula la suma de todos los productos del fabricante Asus.
-#
+select sum(precio)
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre = 'Asus';
+-- 447.99
+
 # 15. Muestra el precio máximo, precio mínimo, precio medio y el número total de productos que tiene el fabricante Crucial.
-#
+select max(precio) as precio_maximo,
+       min(precio) as precio_minimo,
+       avg(precio) as precio_medio,
+       count(*)    as num_productos
+from producto as p
+         join fabricante as f
+              on p.id_fabricante = f.id
+where f.nombre = 'Crucial';
+-- 755,120,437.5,2
+
 # 16. Muestra el número total de productos que tiene cada uno de los fabricantes. El listado también debe incluir los fabricantes que no tienen ningún producto. El resultado mostrará dos columnas, una con el nombre del fabricante y otra con el número de productos que tiene. Ordene el resultado descendentemente por el número de productos.
 #
 # 17. Muestra el precio máximo, precio mínimo y precio medio de los productos de cada uno de los fabricantes. El resultado mostrará el nombre del fabricante junto con los datos que se solicitan.
