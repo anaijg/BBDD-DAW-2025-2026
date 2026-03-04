18b5390dfce454f2ab9c078afd1d0c5083388898
anaijg <ana.jimenezgomez@educa.madrid.org>
2026-03-04 20:36:47 +0100

    ejercicios tienda resumen - todos

--- consultas/empleados/empleados_subconsultas.sql
+++ consultas/empleados/empleados_subconsultas.sql
@@ -1,11 +1,25 @@
+use empleados;
 # 1.2.7 Subconsultas
 # 1.2.7.1 Con operadores básicos de comparación
-# 1. Devuelve un listado con todos los empleados que tiene el departamento de Sistemas. (Sin utilizar INNER JOIN).
-#
-# 2. Devuelve el nombre del departamento con mayor presupuesto y la cantidad que tiene asignada.
-#
+# 1. Devuelve un listado con todos los empleados que tiene el departamento de Sistemas. (Sin utilizar INNER JOIN). Nombre, apellido1 y apellido 2 de los empleados
+select nombre, apellido1, apellido2
+from empleado
+where id_departamento = (select id
+                         from departamento
+                         where nombre = 'Sistemas');
+
+# 2. Devuelve el nombre del departamento con mayor presupuesto y la cantidad que tiene asignada. (Sin utilizar order by ni limit)
+select nombre, presupuesto
+from departamento
+where presupuesto = (select max(presupuesto)
+                     from departamento);
+
 # 3. Devuelve el nombre del departamento con menor presupuesto y la cantidad que tiene asignada.
-#
+select nombre, presupuesto
+from departamento
+where presupuesto = (select min(presupuesto)
+                     from departamento);
+
 # 1.2.7.2 Subconsultas con ALL y ANY
 # 4. Devuelve el nombre del departamento con mayor presupuesto y la cantidad que tiene asignada. Sin hacer uso de MAX, ORDER BY ni LIMIT.
 #
