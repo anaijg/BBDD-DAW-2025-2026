2520029ac6fd25a52a29e9a8b7dcb73b2e4de8cd
Ana Isabel Jimenez Gomez <71371140+anaijg@users.noreply.github.com>
2026-03-04 13:04:15 +0100

    todos los enunciados que faltaban

---
+++ consultas/jardineria/consultas_variadas.sql
@@ -0,0 +1,14 @@
+# 1.4.9 Consultas variadas
+# 1. Devuelve el listado de clientes indicando el nombre del cliente y cuántos pedidos ha realizado. Tenga en cuenta que pueden existir clientes que no han realizado ningún pedido.
+#
+# 2. Devuelve un listado con los nombres de los clientes y el total pagado por cada uno de ellos. Tenga en cuenta que pueden existir clientes que no han realizado ningún pago.
+#
+# 3. Devuelve el nombre de los clientes que hayan hecho pedidos en 2008 ordenados alfabéticamente de menor a mayor.
+#
+# 4. Devuelve el nombre del cliente, el nombre y primer apellido de su representante de ventas y el número de teléfono de la oficina del representante de ventas, de aquellos clientes que no hayan realizado ningún pago.
+#
+# 5. Devuelve el listado de clientes donde aparezca el nombre del cliente, el nombre y primer apellido de su representante de ventas y la ciudad donde está su oficina.
+#
+# 6. Devuelve el nombre, apellidos, puesto y teléfono de la oficina de aquellos empleados que no sean representante de ventas de ningún cliente.
+#
+# 7. Devuelve un listado indicando todas las ciudades donde hay oficinas y el número de empleados que tiene.
---
+++ consultas/ventas/ventas_subconsultas.sql
@@ -0,0 +1,28 @@
+# 1.3.7 Subconsultas
+# 1.3.7.1 Con operadores básicos de comparación
+# 1. Devuelve un listado con todos los pedidos que ha realizado Adela Salas Díaz. (Sin utilizar INNER JOIN).
+#
+# 2. Devuelve el número de pedidos en los que ha participado el comercial Daniel Sáez Vega. (Sin utilizar INNER JOIN)
+#
+# 3. Devuelve los datos del cliente que realizó el pedido más caro en el año 2019. (Sin utilizar INNER JOIN)
+#
+# 4. Devuelve la fecha y la cantidad del pedido de menor valor realizado por el cliente Pepe Ruiz Santana.
+#
+# 5. Devuelve un listado con los datos de los clientes y los pedidos, de todos los clientes que han realizado un pedido durante el año 2017 con un valor mayor o igual al valor medio de los pedidos realizados durante ese mismo año.
+#
+# 1.3.7.2 Subconsultas con ALL y ANY
+# 6. Devuelve el pedido más caro que existe en la tabla pedido si hacer uso de MAX, ORDER BY ni LIMIT.
+#
+# 7. Devuelve un listado de los clientes que no han realizado ningún pedido. (Utilizando ANY o ALL).
+#
+# 8. Devuelve un listado de los comerciales que no han realizado ningún pedido. (Utilizando ANY o ALL).
+#
+# 1.3.7.3 Subconsultas con IN y NOT IN
+# 9. Devuelve un listado de los clientes que no han realizado ningún pedido. (Utilizando IN o NOT IN).
+#
+# 10. Devuelve un listado de los comerciales que no han realizado ningún pedido. (Utilizando IN o NOT IN).
+#
+# 1.3.7.4 Subconsultas con EXISTS y NOT EXISTS
+# 11. Devuelve un listado de los clientes que no han realizado ningún pedido. (Utilizando EXISTS o NOT EXISTS).
+#
+# 12. Devuelve un listado de los comerciales que no han realizado ningún pedido. (Utilizando EXISTS o NOT EXISTS).
