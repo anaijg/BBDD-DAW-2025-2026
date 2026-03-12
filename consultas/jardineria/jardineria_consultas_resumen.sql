5e8984058768cf74575651efedea84e519b35534
anaijg <ana.jimenezgomez@educa.madrid.org>
2026-03-12 17:19:49 +0100

    consultas jardineria

--- consultas/jardineria/jardineria_consultas_resumen.sql
+++ consultas/jardineria/jardineria_consultas_resumen.sql
@@ -1,34 +1,145 @@
 # 1.4.7 Consultas resumen
+use jardineria;
 # 1. ¿Cuántos empleados hay en la compañía?
-#
+select count(codigo_empleado)
+from empleado;
+/*
+ 31
+ */
 # 2. ¿Cuántos clientes tiene cada país?
-#
+
 # 3. ¿Cuál fue el pago medio en 2009?
-#
+select avg(total)
+from pago
+where year(fecha_pago) = '2009';
+/*
+ 4504.076923
+ */
 # 4. ¿Cuántos pedidos hay en cada estado? Ordena el resultado de forma descendente por el número de pedidos.
-#
+SELECT estado, COUNT(*) AS num_pedidos
+FROM pedido
+GROUP BY estado
+ORDER BY num_pedidos DESC;
+/*
+ +---------+-----------+
+|estado   |num_pedidos|
++---------+-----------+
+|Entregado|61         |
+|Pendiente|30         |
+|Rechazado|24         |
++---------+-----------+
+ */
 # 5. Calcula el precio de venta del producto más caro y más barato en una misma consulta.
-#
+SELECT MAX(precio_venta) AS precio_maximo,
+       MIN(precio_venta) AS precio_minimo
+FROM producto;
+/*
+ +-------------+-------------+
+|precio_maximo|precio_minimo|
++-------------+-------------+
+|462.00       |1.00         |
++-------------+-------------+
+
+ */
 # 6. Calcula el número de clientes que tiene la empresa.
-#
+select count(codigo_cliente)
+from cliente;
+/*
+ 36
+ */
 # 7. ¿Cuántos clientes existen con domicilio en la ciudad de Madrid?
-#
+SELECT ciudad, COUNT(codigo_cliente) AS numero_clientes
+FROM cliente
+GROUP BY ciudad
+HAVING ciudad LIKE 'Madrid';
+/*
+ +------+---------------+
+|ciudad|numero_clientes|
++------+---------------+
+|Madrid|11             |
++------+---------------+
+
+ */
 # 8. ¿Calcula cuántos clientes tiene cada una de las ciudades que empiezan por M?
-#
+select count(codigo_cliente), ciudad
+from cliente
+where ciudad like 'M%'
+group by ciudad;
+/*
+ +---------------------+--------------------+
+|count(codigo_cliente)|ciudad              |
++---------------------+--------------------+
+|2                    |Miami               |
+|11                   |Madrid              |
+|1                    |Montornes del valles|
++---------------------+--------------------+
+
+ */
 # 9. Devuelve el nombre de los representantes de ventas y el número de clientes al que atiende cada uno.
-#
+SELECT e.nombre, e.apellido1, COUNT(c.codigo_cliente) AS num_clientes
+FROM empleado e
+         INNER JOIN cliente c ON e.codigo_empleado = c.codigo_empleado_rep_ventas
+GROUP BY e.codigo_empleado, e.nombre, e.apellido1;
+
 # 10. Calcula el número de clientes que no tiene asignado representante de ventas.
-#
+SELECT COUNT(*) AS clientes_sin_representante
+FROM cliente
+WHERE codigo_empleado_rep_ventas IS NULL;
+/*
+ 0
+ */
+SELECT base_imponible,
+       base_imponible * 0.21 AS iva,
+       base_imponible * 1.21 AS total_facturado
+FROM (SELECT SUM(precio_unidad * cantidad) AS base_imponible
+      FROM detalle_pedido) AS facturacion;
 # 11. Calcula la fecha del primer y último pago realizado por cada uno de los clientes. El listado deberá mostrar el nombre y los apellidos de cada cliente.
-#
+select c.nombre_contacto, c.apellido_contacto, min(p.fecha_pago), max(p.fecha_pago)
+from cliente c
+         join pago p
+              on c.codigo_cliente = p.codigo_cliente
+group by c.nombre_contacto, c.apellido_contacto;
+
 # 12. Calcula el número de productos diferentes que hay en cada uno de los pedidos.
-#
+SELECT codigo_pedido, count(codigo_producto)
+FROM detalle_pedido
+GROUP BY codigo_pedido, codigo_producto
+order by codigo_pedido;
+
+
 # 13. Calcula la suma de la cantidad total de todos los productos que aparecen en cada uno de los pedidos.
-#
+select pedido.codigo_pedido,
+       fecha_pedido,
+       sum(detalle_pedido.cantidad)             as cantidad_de_productos,
+       group_concat(distinct (producto.nombre)) as productos_del_pedido
+from pedido
+         inner join detalle_pedido on pedido.codigo_pedido = detalle_pedido.codigo_pedido
+         inner join producto on detalle_pedido.codigo_producto = producto.codigo_producto
+group by pedido.codigo_pedido;
 # 14. Devuelve un listado de los 20 productos más vendidos y el número total de unidades que se han vendido de cada uno. El listado deberá estar ordenado por el número total de unidades vendidas.
-#
+SELECT p.nombre, SUM(dp.cantidad) AS total_unidades_vendidas
+FROM producto p
+         INNER JOIN detalle_pedido dp ON p.codigo_producto = dp.codigo_producto
+GROUP BY p.codigo_producto, p.nombre
+ORDER BY total_unidades_vendidas DESC
+LIMIT 20;
 # 15. La facturación que ha tenido la empresa en toda la historia, indicando la base imponible, el IVA y el total facturado. La base imponible se calcula sumando el coste del producto por el número de unidades vendidas de la tabla detalle_pedido. El IVA es el 21 % de la base imponible, y el total la suma de los dos campos anteriores.
-#
+SELECT SUM(precio_unidad * cantidad)        AS base_imponible,
+       SUM(precio_unidad * cantidad) * 0.21 AS iva,
+       SUM(precio_unidad * cantidad) * 1.21 AS total_facturado
+FROM detalle_pedido;
+/*
+ +--------------+----------+---------------+
+|base_imponible|iva       |total_facturado|
++--------------+----------+---------------+
+|217738.00     |45724.9800|263462.9800    |
++--------------+----------+---------------+
+
+ */
+SELECT SUM(precio_unidad * cantidad)                                        AS base_imponible,
+       SUM(precio_unidad * cantidad) * 0.21                                 AS iva,
+       SUM(precio_unidad * cantidad) + SUM(precio_unidad * cantidad) * 0.21 AS total_facturado
+FROM detalle_pedido;
 # 16. La misma información que en la pregunta anterior, pero agrupada por código de producto.
 #
 # 17. La misma información que en la pregunta anterior, pero agrupada por código de producto filtrada por los códigos que empiecen por OR.
@@ -36,3 +147,6 @@
 # 18. Lista las ventas totales de los productos que hayan facturado más de 3000 euros. Se mostrará el nombre, unidades vendidas, total facturado y total facturado con impuestos (21% IVA).
 #
 # 19. Muestre la suma total de todos los pagos que se realizaron para cada uno de los años que aparecen en la tabla pagos.
+SELECT YEAR(fecha_pago) AS año, SUM(total) AS suma_total_pagos
+FROM pago
+GROUP BY YEAR(fecha_pago);
--- consultas/jardineria/jardineria_unitabla.sql
+++ consultas/jardineria/jardineria_unitabla.sql
@@ -1,39 +1,219 @@
+use jardineria;
 # 1.4.4 Consultas sobre una tabla
 # 1. Devuelve un listado con el código de oficina y la ciudad donde hay oficinas.
-#
+select codigo_oficina, ciudad
+from oficina;
+/*
+ +--------------+--------------------+
+|codigo_oficina|ciudad              |
++--------------+--------------------+
+|BCN-ES        |Barcelona           |
+|BOS-USA       |Boston              |
+|LON-UK        |Londres             |
+|MAD-ES        |Madrid              |
+|PAR-FR        |Paris               |
+|SFC-USA       |San Francisco       |
+|SYD-AU        |Sydney              |
+|TAL-ES        |Talavera de la Reina|
+|TOK-JP        |Tokyo               |
++--------------+--------------------+
+
+ */
 # 2. Devuelve un listado con la ciudad y el teléfono de las oficinas de España.
-#
+select ciudad, telefono
+from oficina;
+/*
+ +--------------------+---------------+
+|ciudad              |telefono       |
++--------------------+---------------+
+|Barcelona           |+34 93 3561182 |
+|Boston              |+1 215 837 0825|
+|Londres             |+44 20 78772041|
+|Madrid              |+34 91 7514487 |
+|Paris               |+33 14 723 4404|
+|San Francisco       |+1 650 219 4782|
+|Sydney              |+61 2 9264 2451|
+|Talavera de la Reina|+34 925 867231 |
+|Tokyo               |+81 33 224 5000|
++--------------------+---------------+
+
+ */
 # 3. Devuelve un listado con el nombre, apellidos y email de los empleados cuyo jefe tiene un código de jefe igual a 7.
-#
+SELECT nombre, apellido1, apellido2, email
+FROM empleado
+WHERE codigo_jefe LIKE '7';
 # 4. Devuelve el nombre del puesto, nombre, apellidos y email del jefe de la empresa.
-#
+SELECT puesto, nombre, apellido1, apellido2, email
+FROM empleado
+WHERE codigo_jefe IS NULL;
+/*
+ +----------------+------+---------+---------+--------------------+
+|puesto          |nombre|apellido1|apellido2|email               |
++----------------+------+---------+---------+--------------------+
+|Director General|Marcos|Magaña   |Perez    |marcos@jardineria.es|
++----------------+------+---------+---------+--------------------+
+
+ */
 # 5. Devuelve un listado con el nombre, apellidos y puesto de aquellos empleados que no sean representantes de ventas.
-#
+SELECT nombre, apellido1, apellido2, puesto
+FROM empleado
+WHERE puesto NOT LIKE 'Representante Ventas';
+/*
+ +--------+----------+---------+---------------------+
+|nombre  |apellido1 |apellido2|puesto               |
++--------+----------+---------+---------------------+
+|Marcos  |Magaña    |Perez    |Director General     |
+|Ruben   |López     |Martinez |Subdirector Marketing|
+|Alberto |Soria     |Carrasco |Subdirector Ventas   |
+|Maria   |Solís     |Jerez    |Secretaria           |
+|Carlos  |Soria     |Jimenez  |Director Oficina     |
+|Emmanuel|Magaña    |Perez    |Director Oficina     |
+|Francois|Fignon    |         |Director Oficina     |
+|Michael |Bolton    |         |Director Oficina     |
+|Hilary  |Washington|         |Director Oficina     |
+|Nei     |Nishikori |         |Director Oficina     |
+|Amy     |Johnson   |         |Director Oficina     |
+|Kevin   |Fallmer   |         |Director Oficina     |
++--------+----------+---------+---------------------+
+
+ */
 # 6. Devuelve un listado con el nombre de los todos los clientes españoles.
-#
+SELECT *
+FROM cliente
+WHERE pais LIKE 'Spain';
 # 7. Devuelve un listado con los distintos estados por los que puede pasar un pedido.
-#
+
+select distinct(estado) as estados_de_pedido
+from pedido;
+/*
+ +-----------------+
+|estados_de_pedido|
++-----------------+
+|Entregado        |
+|Rechazado        |
+|Pendiente        |
++-----------------+
+
+ */
 # 8. Devuelve un listado con el código de cliente de aquellos clientes que realizaron algún pago en 2008. Tenga en cuenta que deberá eliminar aquellos códigos de cliente que aparezcan repetidos. Resuelva la consulta:
-#
+SELECT DISTINCT codigo_cliente
+FROM pago
+WHERE YEAR(fecha_pago) = 2008;
+/*
+ +--------------+
+|codigo_cliente|
++--------------+
+|1             |
+|13            |
+|14            |
+|26            |
++--------------+
+
+ */
 # a) Utilizando la función YEAR de MySQL.
 # b) Utilizando la función DATE_FORMAT de MySQL.
 # c) Sin utilizar ninguna de las funciones anteriores.
 
 # 9. Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos que no han sido entregados a tiempo.
-#
+SELECT codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega
+FROM pedido
+WHERE TIMESTAMPDIFF(DAY, fecha_entrega, fecha_esperada) < 0;
+-- (también valdría poner where fecha_entrega > fecha_esperada)
+
 # 10. Devuelve un listado con el código de pedido, código de cliente, fecha esperada y fecha de entrega de los pedidos cuya fecha de entrega ha sido al menos dos días antes de la fecha esperada.
-#
+
 # a) Utilizando la función ADDDATE de MySQL.
+select codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega
+from pedido
+where fecha_entrega <= adddate(fecha_esperada, -2);
+
 # b) Utilizando la función DATEDIFF de MySQL.
+select codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega
+from pedido
+where datediff(fecha_esperada, fecha_entrega) >= 2;
+
+SELECT codigo_pedido,
+       codigo_cliente,
+       fecha_esperada,
+       fecha_entrega,
+       DATEDIFF(fecha_esperada, fecha_entrega) AS diferencia
+FROM pedido
+GROUP BY codigo_pedido
+HAVING diferencia >= 2;
+
+
 # c) ¿Sería posible resolver esta consulta utilizando el operador de suma + o resta -?
+select codigo_pedido, codigo_cliente, fecha_esperada, fecha_entrega
+from pedido
+where fecha_entrega <= fecha_esperada - 2;
+/*
+ +-------------+--------------+--------------+-------------+
+|codigo_pedido|codigo_cliente|fecha_esperada|fecha_entrega|
++-------------+--------------+--------------+-------------+
+|2            |5             |2007-10-28    |2007-10-26   |
+|24           |14            |2008-07-31    |2008-07-25   |
+|30           |13            |2008-09-03    |2008-08-31   |
+|36           |14            |2008-12-15    |2008-12-10   |
+|53           |13            |2008-11-15    |2008-11-09   |
+|89           |35            |2007-12-13    |2007-12-10   |
+|91           |27            |2009-03-29    |2009-03-27   |
+|93           |27            |2009-05-30    |2009-05-17   |
++-------------+--------------+--------------+-------------+
+
+ */
 # 11. Devuelve un listado de todos los pedidos que fueron rechazados en 2009.
-#
+select *
+from pedido
+where estado = 'Rechazado'
+  and year(fecha_pedido) = '2009';
+
 # 12. Devuelve un listado de todos los pedidos que han sido entregados en el mes de enero de cualquier año.
-#
+SELECT *
+FROM pedido
+WHERE MONTH(fecha_entrega) = 1;
+
 # 13. Devuelve un listado con todos los pagos que se realizaron en el año 2008 mediante Paypal. Ordene el resultado de mayor a menor.
-#
+SELECT *
+FROM pago
+WHERE YEAR(fecha_pago) = 2008
+  AND forma_pago LIKE 'PayPal'
+ORDER BY total DESC;
+/*
+ +--------------+----------+--------------+----------+--------+
+|codigo_cliente|forma_pago|id_transaccion|fecha_pago|total   |
++--------------+----------+--------------+----------+--------+
+|26            |PayPal    |ak-std-000020 |2008-03-18|18846.00|
+|14            |PayPal    |ak-std-000015 |2008-07-15|4160.00 |
+|13            |PayPal    |ak-std-000014 |2008-08-04|2246.00 |
+|1             |PayPal    |ak-std-000001 |2008-11-10|2000.00 |
+|1             |PayPal    |ak-std-000002 |2008-12-10|2000.00 |
++--------------+----------+--------------+----------+--------+
+
+ */
+
 # 14. Devuelve un listado con todas las formas de pago que aparecen en la tabla pago. Tenga en cuenta que no deben aparecer formas de pago repetidas.
-#
+SELECT DISTINCT (forma_pago)
+FROM pago;
+/*
+ +-------------+
+|forma_pago   |
++-------------+
+|PayPal       |
+|Transferencia|
+|Cheque       |
++-------------+
+
+ */
 # 15. Devuelve un listado con todos los productos que pertenecen a la gama Ornamentales y que tienen más de 100 unidades en stock. El listado deberá estar ordenado por su precio de venta, mostrando en primer lugar los de mayor precio.
-#
+select *
+from producto
+where gama = 'Ornamentales'
+  and cantidad_en_stock > 100
+order by precio_venta desc;
+
+
 # 16. Devuelve un listado con todos los clientes que sean de la ciudad de Madrid y cuyo representante de ventas tenga el código de empleado 11 o 30.
+SELECT *
+FROM cliente
+WHERE ciudad = 'Madrid'
+  AND codigo_empleado_rep_ventas IN (11, 30);
