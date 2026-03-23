9249ef3e3fcb74dabf6221a07aa6db21af7c6cb8
Ana Isabel Jimenez Gomez <71371140+anaijg@users.noreply.github.com>
2026-03-23 19:17:45 +0100

    Add SQL queries for employee and client conditions
    Added SQL queries to retrieve employees not representing clients and clients with no payments.

--- consultas/jardineria/jardineria_subconsultas.sql
+++ consultas/jardineria/jardineria_subconsultas.sql
@@ -82,15 +82,38 @@ WHERE cantidad_en_stock <= ALL (SELECT cantidad_en_stock FROM producto);
   
 # 1.4.8.3 Subconsultas con IN y NOT IN
 # 11. Devuelve el nombre, apellido1 y cargo de los empleados que no representen a ningún cliente.
+SELECT nombre, apellido1, puesto
+FROM empleado
+WHERE codigo_empleado NOT IN (SELECT codigo_empleado_rep_ventas
+                              FROM cliente
+                              WHERE codigo_empleado_rep_ventas IS NOT NULL);
 
 # 12. Devuelve un listado que muestre solamente los clientes que no han realizado ningún pago.
-#
+SELECT *
+FROM cliente
+WHERE codigo_cliente NOT IN (SELECT codigo_cliente FROM pago);
+
 # 13. Devuelve un listado que muestre solamente los clientes que sí han realizado algún pago.
-#
+SELECT *
+FROM cliente
+WHERE codigo_cliente IN (SELECT codigo_cliente FROM pago);
+
 # 14. Devuelve un listado de los productos que nunca han aparecido en un pedido.
-#
+SELECT *
+FROM producto
+WHERE codigo_producto NOT IN (SELECT codigo_producto FROM detalle_pedido);
+
 # 15. Devuelve el nombre, apellidos, puesto y teléfono de la oficina de aquellos empleados que no sean representante de ventas de ningún cliente.
-#
+SELECT e.nombre,
+       e.apellido1,
+       e.apellido2,
+       e.puesto,
+       (SELECT telefono FROM oficina o WHERE o.codigo_oficina = e.codigo_oficina) AS telefono
+FROM empleado e
+WHERE e.codigo_empleado NOT IN (SELECT codigo_empleado_rep_ventas
+                                FROM cliente
+                                WHERE codigo_empleado_rep_ventas IS NOT NULL);
+
 # 16. Devuelve las oficinas donde no trabajan ninguno de los empleados que hayan sido los representantes de ventas de algún cliente que haya realizado la compra de algún producto de la gama Frutales.
 #
 # 17. Devuelve un listado con los clientes que han realizado algún pedido pero no han realizado ningún pago.
